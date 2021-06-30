c9d2269542eea4853a422721ce1fed3eebdde458
Kyle Mayer <x@kylemayer.dev>
2021-06-30 14:29:33 -0700

    fetch functions

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -2,7 +2,7 @@ import request from 'superagent';
 
 const URL = 'https://arcane-journey-95948.herokuapp.com';
 
-const async function signup(email, password) {
+export async function signup(email, password) {
     const data = await request
         .post(`${URL}/auth/signup`)
         .send({
@@ -13,22 +13,40 @@ const async function signup(email, password) {
 }
 
 
-const async function login() {
+export async function login(email, password) {
     const data = await request
-
+        .post(`${URL}/auth/signin`)
+        .send({
+            email: email,
+            password: password
+        })
+    return data.body.token;
 }
 
-const async function getTodos() {
+export async function getTodos(token) {
     const data = await request
+    .get(`${URL}/api/todos`)
+    .set('Authorization', token)
 
+    return data.body;
 }
 
-const async function getTodo() {
+export async function getTodo(todo, token) {
     const data = await request
+        .post(`${URL}/api/todos`)
+        .send({
+            todo: todo,
+            completed: false,
+        })
+        .set('Authorization', token)
 
+        return data.body;
 }
 
-const async function complete() {
+export async function complete(id, token) {
     const data = await request
+        .put(`${URL}/api/todos/${id}`)
+        .set('Authorization', token)
 
+        return data.body;
 }
